55759b40e751ae73981a9188caeb61f7b54a1607
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-05-23 21:59:49 -0700

    writing encryption tests

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
 
 var UserSchema = new mongoose.Schema({
   username: String,
@@ -15,4 +16,24 @@ var UserSchema = new mongoose.Schema({
   progress: Number
 });
 
+UserSchema.statics.generateSalt = function() {
+  return crypto.randomBytes(16).toString('base64');
+}
+
+UserSchema.statics.encryptPassword = function(plainText, salt) {
+  var hash = crypto.createHash('sha1');
+  hash.update(plainText);
+  hash.update(salt);
+  return hash.digest('hex');
+};
+
+UserSchema.pre('save', function(next) {
+  if (!this.salt) this.salt = this.constructor.generateSalt();
+  if (this.isModified('password')) {
+    this.password = this.constructor.encryptPassword(this.password, this.salt);
+  }
+  next();
+});
+
 mongoose.model('User', UserSchema);
+
--- tests/server/models/user.test.js
+++ tests/server/models/user.test.js
@@ -3,6 +3,7 @@ var clearDB = require('mocha-mongoose')(dbURI);
 
 var expect = require('chai').expect;
 var mongoose = require('mongoose');
+var sinon = require('sinon');
 
 // require in models
 var models = require('../../../server/models');
@@ -44,14 +45,75 @@ describe("User model", function() {
 
     describe("encryption", function() {
 
-      describe('generateSalt method', function () {
+      describe('generateSalt static', function () {
 
         it("should exist", function(){
           expect(User.generateSalt).to.be.a('function');
         });
 
-        
+        it('should return a random string', function() {
+          var string1 = User.generateSalt();
+          var string2 = User.generateSalt();
+          expect(string1).to.be.a('string');
+          expect(string2).to.be.a('string');
+          expect(string1).to.not.equal(string2);
+        });
 
+      });
+
+      describe("encryptPassword static", function(){
+
+        var cryptoStub, hashUpdateSpy, hashDigestStub;
+        beforeEach('setup hash and cryptostubs and spies', function() {
+          cryptoStub = sinon.stub(require('crypto'), 'createHash');
+          hashUpdateSpy = sinon.spy();
+          hashDigestStub = sinon.stub();
+          cryptoStub.returns({
+            update: hashUpdateSpy,
+            digest: hashDigestStub
+          });
+        });
+
+        afterEach('restore stubs', function(){
+          cryptoStub.restore();
+        });
+
+        it('should exist', function() {
+          expect(User.encryptPassword).to.be.a('function');
+        });
+
+        it('should call crypto.createHash with "sha1" method', function() {
+          User.encryptPassword('asd', 'jkl');
+          expect(cryptoStub.calledWith('sha1')).to.be.ok;
+        });
+
+        it('should call hash.update with the first and second argument', function() {
+          var pass = 'password';
+          var salt = 'qwertyuiop';
+          User.encryptPassword(pass, salt);
+          expect(hashUpdateSpy.getCall(0).args[0]).to.be.equal(pass);
+          expect(hashUpdateSpy.getCall(1).args[0]).to.be.equal(salt);
+        });
+
+        it('should call hash.digest with hex and return the result', function() {
+          var x = {};
+          hashDigestStub.returns(x);
+
+          var e = User.encryptPassword('asdf', 'qwert');
+          expect(hashDigestStub.calledWith('hex')).to.be.ok;
+          expect(e).to.be.equal(x);
+        })
+
+
+      });
+
+      describe("on user creation", function() {
+
+        var encryptSpy, saltSpy;
+
+        var createUser = function(){
+          return User.create({email: 'test@test.test', username: 'testuser', password: '12345'});
+        }
       });
 
 
